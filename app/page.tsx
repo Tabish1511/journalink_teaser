693e1e43e65ea0becfc6c109dee07a9c1658b2b6
Tabish1511 <khaqantabish@gmail.com>
2024-09-17 19:58:45 +0200

    fix the code to look like the 'cover' component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { Cover } from "@/components/cover";
+import { CoverDemo } from "@/components/warp";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       Hello from tabish
-      <Cover/>
+      <CoverDemo/>
     </div>
   );
 }
