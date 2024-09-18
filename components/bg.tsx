c3efe0059abddc15cc03353b1ee4c62d1cd4c89c
Tabish1511 <khaqantabish@gmail.com>
2024-09-18 17:03:33 +0200

    fix 'coming soon'

--- components/bg.tsx
+++ components/bg.tsx
@@ -1,20 +1,33 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BackgroundBeams } from "../components/background-beams";
 import { CoverDemo } from "./warp";
+import { TextGenerateEffectDemo } from "./comingSoon";
 
 export function BackgroundBeamsDemo() {
-  return (
+    const [showTextGenerateEffect, setShowTextGenerateEffect] = useState(false);  
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          setShowTextGenerateEffect(true);
+        }, 2000); // 2000 milliseconds = 2 seconds
+    
+        return () => clearTimeout(timer); // Cleanup the timer
+      }, []);
+
+    return (
     <div className="h-[40rem] w-full rounded-md bg-neutral-950 flex antialiased">
-      <div className="max-w-4xl mx-auto p-4">
+        <div className="max-w-4xl mx-auto p-4 flex flex-col items-center">
         <CoverDemo />
-      </div>
-      <div className="flex flex-col justify-center align-center">
-      <BackgroundBeams />
-      comming soon
-      </div>
+        {showTextGenerateEffect && (
+          <div className="text-center mt-4">
+            <TextGenerateEffectDemo />
+          </div>
+        )}
+        </div>
+        <BackgroundBeams />
     </div>
-  );
+    );
 }
 
 
---
+++ components/comingSoon.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { TextGenerateEffect } from "../components/text-generate-effect";
+
+const words = `Coming soon...`;
+
+export function TextGenerateEffectDemo() {
+  return <TextGenerateEffect words={words} />;
+}
